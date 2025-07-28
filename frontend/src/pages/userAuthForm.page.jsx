bc1a5a5293abe1602d25a310cfcbd5abc93e36d8
Adnan-Shaikh-Dev <adnanshaikhcareer@gmail.com>
2025-07-28 21:48:55 +0530

    Added some animations for UI of signin and signup form using Framer Motion

--- frontend/src/pages/userAuthForm.page.jsx
+++ frontend/src/pages/userAuthForm.page.jsx
@@ -1,62 +1,65 @@
 import { Link } from "react-router-dom";
 import InputBox from "../components/input.component";
 import googleIcon from "../imgs/google.png";
+import AnimationWrapper from "../common/page-animation";
 
 const UserAuthForm = ({ type }) => {
   return (
-    <section className="h-cover flex items-center justify-center">
-      <form className="w-[80%] max-w-[400px]">
-        <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
-          {type === "sign-in" ? "Welcome Back" : "Join us today"}
-        </h1>
-        {type != "sign-in" ? (
+    <AnimationWrapper keyValue={type}>
+      <section className="h-cover flex items-center justify-center">
+        <form className="w-[80%] max-w-[400px]">
+          <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
+            {type === "sign-in" ? "Welcome Back" : "Join us today"}
+          </h1>
+          {type != "sign-in" ? (
+            <InputBox
+              name="fullname"
+              type="text"
+              placeholder="Your Full Name"
+              icon="fi-rr-user"
+            />
+          ) : (
+            ""
+          )}
           <InputBox
             name="fullname"
             type="text"
-            placeholder="Your Full Name"
-            icon="fi-rr-user"
+            placeholder="Your Email"
+            icon="fi-rr-envelope"
           />
-        ) : (
-          ""
-        )}
-        <InputBox
-          name="fullname"
-          type="text"
-          placeholder="Your Email"
-          icon="fi-rr-envelope"
-        />
-        <InputBox
-          name="password"
-          type="password"
-          placeholder="Your Password"
-          icon="fi-rr-key"
-        />
+          <InputBox
+            name="password"
+            type="password"
+            placeholder="Your Password"
+            icon="fi-rr-key"
+          />
 
-        <button className="btn-dark center mt-14" type="submit">
-          {type.replace("-", " ")}
-        </button>
+          <button className="btn-dark center mt-14" type="submit">
+            {type.replace("-", " ")}
+          </button>
 
-        <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
-          <hr className="w-1/2 border-black" />
-          <p>or</p>
-          <hr className="w-1/2 border-black" />
-        </div>
-        <button className="btn-dark flex items-center justify-center gap-4 w-[90%] center">
-          <img src={googleIcon} className="w-5" />
-          continue with google
-        </button>
-        {type == "sign-in" ? (
-          <p className="mt-6 text-dark-grey text-xl text-center">
-            Don't have an account?{" "}
-            <Link to="/signup" className="underline text-black text-xl ml-1">
-              Join us today
-            </Link>
-          </p>
-        ) : (
-          ""
-        )}
-      </form>
-    </section>
+          <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
+            <hr className="w-1/2 border-black" />
+            <p>or</p>
+            <hr className="w-1/2 border-black" />
+          </div>
+          <button className="btn-dark flex items-center justify-center gap-4 w-[90%] center">
+            <img src={googleIcon} className="w-5" />
+            continue with google
+          </button>
+          {type == "sign-in" ? (
+            <p className="mt-6 text-dark-grey text-xl text-center">
+              Don't have an account?{" "}
+              <Link to="/signup" className="underline text-black text-xl ml-1">
+                Join us today
+              </Link>
+            </p>
+          ) : (
+            ""
+          )}
+        </form>
+      </section>
+    </AnimationWrapper>
   );
 };
 
