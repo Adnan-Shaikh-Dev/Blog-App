7e830ac4d0e47d0a58652ce205b49a840141c44a
Adnan-Shaikh-Dev <adnanshaikhcareer@gmail.com>
2025-08-07 19:37:09 +0530

    Basic editor publisher form setup

---
+++ frontend/src/components/blog-editor.component.jsx
@@ -0,0 +1,5 @@
+const BlogEditor = () => {
+  return <h1>Blog Editor from Component JSX File</h1>;
+};
+
+export default BlogEditor;
---
+++ frontend/src/components/publish-form.component.jsx
@@ -0,0 +1,5 @@
+const PublishForm = () => {
+  return <h1>Publish Form from Component JSX File</h1>;
+};
+
+export default PublishForm;
