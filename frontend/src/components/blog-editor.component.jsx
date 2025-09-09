4060e8c1d1d47a8fb1edf552708e8d109d038d15
Adnan-Shaikh-Dev <adnanshaikhcareer@gmail.com>
2025-09-09 14:24:50 +0530

    Updated AWS creds

--- frontend/src/components/blog-editor.component.jsx
+++ frontend/src/components/blog-editor.component.jsx
@@ -3,11 +3,34 @@ import logo from "../imgs/NuclearNestLogo.png";
 import AnimationWrapper from "../common/page-animation";
 import defaultBanner from "../imgs/blog banner.png";
 import { uploadImage } from "../common/aws";
-import { useRef } from "react";
+import { useContext, useRef, useEffect } from "react";
+import { tools } from "./tools.component";
 import { Toaster, toast } from "react-hot-toast";
+import Editor, { EditorContext } from "../pages/editor.pages";
+import EditorJS from "@editorjs/editorjs";
 
 const BlogEditor = () => {
   let blogBannerRef = useRef();
+
+  //UseEffect
+  useEffect(() => {
+    setTextEditor(
+      new EditorJS({
+        holderId: "textEditor",
+        data: "",
+        tools,
+        placeholder: "Let's write an awesome story",
+      })
+    );
+  }, []);
+
+  let {
+    blog,
+    blog: { title, banner, content, tags, des },
+    setBlog,
+    textEditor,
+    setTextEditor,
+  } = useContext(EditorContext);
   const handleBannerUpload = (e) => {
     // console.log(e);
     let img = e.target.files[0];
@@ -19,7 +42,8 @@ const BlogEditor = () => {
           if (url) {
             toast.dismiss(loadingToast);
             toast.success("Uploaded");
-            blogBannerRef.current.src = url;
+
+            setBlog({ ...blog, banner: url });
           }
         })
         .catch((err) => {
@@ -29,15 +53,58 @@ const BlogEditor = () => {
     }
   };
 
+  const handleTitleKeyDown = (e) => {
+    console.log(e);
+    if (e.keyCode == 13) {
+      //enter key
+      e.preventDefault(e);
+    }
+  };
+
+  const handleTitleChange = (e) => {
+    let input = e.target;
+
+    input.style.height = "auto";
+    input.style.height = input.scrollHeight + "px";
+
+    setBlog({ ...blog, title: input.value });
+  };
+
+  const handleError = (e) => {
+    let img = e.target;
+    img.src = defaultBanner;
+    console.log(img);
+  };
+
+  const handlePublishEvent = () => {
+    if (!banner.length) {
+      return toast.error("Upload a blog banner to publish it");
+    }
+
+    if (!title.length) {
+      return toast.error("Write a title for the blog to publish it");
+    }
+
+    if (textEditor.isReady) {
+      textEditor.save().then((data) => {
+        console.log(data);
+      });
+    }
+  };
+
   return (
     <>
       <nav className="navbar">
         <Link to="/" className="flex-none w-20">
           <img src={logo} alt="" />
         </Link>
-        <p className="max-md:hidden text-black line-clamp-1 w-full">New Blog</p>
+        <p className="max-md:hidden text-black line-clamp-1 w-full">
+          {title.length ? title : "New Blog"}
+        </p>
         <div className="flex gap-4 ml-auto">
-          <button className="btn-dark py-2">Publish</button>
+          <button className="btn-dark py-2" onClick={handlePublishEvent}>
+            Publish
+          </button>
           <button className="btn-light py-2">Save Draft</button>
         </div>
       </nav>
@@ -47,7 +114,7 @@ const BlogEditor = () => {
           <div className="mx-auto max-w-[900px] w-full">
             <div className="relative aspect-video bg-white border-4 border-grey ">
               <label htmlFor="uploadBanner">
-                <img src={defaultBanner} className="z-20" ref={blogBannerRef} />
+                <img src={banner} className="z-20" onError={handleError} />
                 <input
                   id="uploadBanner"
                   type="file"
@@ -57,6 +124,15 @@ const BlogEditor = () => {
                 />
               </label>
             </div>
+            <textarea
+              placeholder="Blog Title"
+              className="text-4xl font-medium w-full h-20 outline-none resize-none mt-10 leading-tight placeholder:opacity-40"
+              onKeyDown={handleTitleKeyDown}
+              onChange={handleTitleChange}
+            ></textarea>
+            <hr className="w-full opacity-10 my-5" />
+
+            <div id="textEditor" className="font-gelasio"></div>
           </div>
         </section>
       </AnimationWrapper>
--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ mongoose.connect(process.env.DB_LOCATION, { autoIndex: true });
 
 // setup of s3 bucket
 const s3 = new aws.S3({
-  region: "us-east-1",
+  region: "ap-south-1",
   accessKeyId: process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
@@ -45,7 +45,7 @@ const generateUploadUrl = async () => {
   const date = new Date();
   const imageName = `${nanoid()}-${date.getTime()}.jpeg`;
   return await s3.getSignedUrlPromise("putObject", {
-    Bucket: "myblogappbucket090103",
+    Bucket: "my-blogging-website-bucket",
     Key: imageName,
     Expires: 1000,
     ContentType: "image/jpeg",
