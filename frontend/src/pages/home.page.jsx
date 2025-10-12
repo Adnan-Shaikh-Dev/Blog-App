0ed4cb468640e88fb0aedaac6d8c63a33993ac14
Adnan-Shaikh-Dev <adnanshaikhcareer@gmail.com>
2025-10-12 12:04:43 +0530

    Added trending blogs functionality to front-end

--- frontend/src/pages/home.page.jsx
+++ frontend/src/pages/home.page.jsx
@@ -1,50 +1,88 @@
-import React, { useEffect, useState } from 'react'
-import InPageNavigation from '../components/inpage-navigation.component'
-import AnimationWrapper from '../common/page-animation'
-import axios from 'axios';
-import Loader from '../components/loader.component'
-import BlogPostCard from '../components/blog-post.component';
+import React, { useEffect, useState } from "react";
+import InPageNavigation from "../components/inpage-navigation.component";
+import AnimationWrapper from "../common/page-animation";
+import axios from "axios";
+import Loader from "../components/loader.component";
+import BlogPostCard from "../components/blog-post.component";
+import MinimalBlogPost from "../components/nobanner-blog-post.component";
 const Homepage = () => {
-    let [blogs, setBlog] = useState(null) 
-    const fetchLatestBlogs = () =>{
-        axios.get(import.meta.env.VITE_SERVER_DOMAIN + '/latest-blogs')
-        .then(({data})=>{
-            setBlog(data.blogs)
-        }).catch(err =>{
-            console.log(err)
-        })
-    }
+  let [blogs, setBlog] = useState(null);
+  let [trendingBlogs, setTrendingBlogs] = useState(null);
+  const fetchLatestBlogs = () => {
+    axios
+      .get(import.meta.env.VITE_SERVER_DOMAIN + "/latest-blogs")
+      .then(({ data }) => {
+        setBlog(data.blogs);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const fetchTrendingBlogs = () => {
+    axios
+      .get(import.meta.env.VITE_SERVER_DOMAIN + "/trending-blogs")
+      .then(({ data }) => {
+        setTrendingBlogs(data.blogs);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    useEffect(()=>{
-        fetchLatestBlogs()
-    },[])
+  useEffect(() => {
+    fetchLatestBlogs();
+    fetchTrendingBlogs();
+  }, []);
   return (
     <AnimationWrapper>
-        <section className='h-cover flex justify-center gap-10'>
-            <div className='w-full'>
-                {/* {latest blogs} */}
-                <InPageNavigation routes={["home", "trending blogs"]} defaultHidden={["trending blogs"]}>
+      <section className="h-cover flex justify-center gap-10">
+        <div className="w-full">
+          {/* {latest blogs} */}
+          <InPageNavigation
+            routes={["home", "trending blogs"]}
+            defaultHidden={["trending blogs"]}
+          >
+            <>
+              {blogs === null ? (
+                <Loader />
+              ) : (
+                Array.isArray(blogs) &&
+                blogs.map((blog, i) => {
+                  return (
+                    <AnimationWrapper
+                      key={i}
+                      transition={{ duration: 1, delay: i * 0.1 }}
+                    >
+                      <BlogPostCard
+                        content={blog}
+                        author={blog.author.personal_info}
+                      />
+                    </AnimationWrapper>
+                  );
+                })
+              )}
+            </>
 
-                    <>
-                    {
-                        blogs === null ? <Loader/> :
-                       Array.isArray(blogs)&& blogs.map((blog,i)=>{
-                        return <AnimationWrapper key={i} transition={{duration:1, delay:i*.1}}>
-                            <BlogPostCard content={blog} author={blog.author.personal_info}/>
-                        </AnimationWrapper>
-                    })
-                    }
-                    </>
-
-                    <h1>Trending Blogs here</h1>
-                </InPageNavigation>
-            </div>
-            <div>
-                {/* {Filters and trending blogs} */}
-            </div>
-        </section>
+            {trendingBlogs == null ? (
+              <Loader />
+            ) : (
+              trendingBlogs.map((blog, i) => {
+                return (
+                  <AnimationWrapper
+                    transition={{ duration: 1, delay: i * 0.1 }}
+                    key={i}
+                  >
+                    <MinimalBlogPost blog={blog} index={i} />
+                  </AnimationWrapper>
+                );
+              })
+            )}
+          </InPageNavigation>
+        </div>
+        <div>{/* {Filters and trending blogs} */}</div>
+      </section>
     </AnimationWrapper>
-)
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
