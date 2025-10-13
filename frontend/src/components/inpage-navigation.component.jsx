9d599f16f6732a7ec60044418168d396104230ab
Adnan-Shaikh-Dev <adnanshaikhcareer@gmail.com>
2025-10-13 21:45:34 +0530

    Added category wise filter front end functionality

--- frontend/src/components/inpage-navigation.component.jsx
+++ frontend/src/components/inpage-navigation.component.jsx
@@ -1,40 +1,56 @@
-import React,{useState,useRef, useEffect} from 'react'
+import React, { useState, useRef, useEffect } from "react";
 
-const InPageNavigation = ({routes,defaultHidden = [],defaultActiveIndex = 0,children}) => {
- let [inPageNavIndex, setInPageNavIndex] = useState(defaultActiveIndex);
- let activeTabLineRef = useRef()
- let activeTabRef = useRef()
- const changePageState = (btn, i) =>{
-    console.log(btn,i)
-    let {offsetWidth, offsetLeft} = btn;
-    activeTabLineRef.current.style.width = offsetWidth+"px"
-    activeTabLineRef.current.style.left = offsetLeft+"px"
+export let activeTabLineRef;
+export let activeTabRef;
 
-    setInPageNavIndex(i)
- }
+const InPageNavigation = ({
+  routes,
+  defaultHidden = [],
+  defaultActiveIndex = 0,
+  children,
+}) => {
+  let [inPageNavIndex, setInPageNavIndex] = useState(defaultActiveIndex);
+  activeTabLineRef = useRef();
+  activeTabRef = useRef();
+  const changePageState = (btn, i) => {
+    console.log(btn, i);
+    let { offsetWidth, offsetLeft } = btn;
+    activeTabLineRef.current.style.width = offsetWidth + "px";
+    activeTabLineRef.current.style.left = offsetLeft + "px";
 
- useEffect(()=>{
-    changePageState(activeTabRef.current,defaultActiveIndex)
- },[])
+    setInPageNavIndex(i);
+  };
+
+  useEffect(() => {
+    changePageState(activeTabRef.current, defaultActiveIndex);
+  }, []);
   return (
     <>
-    <div className='relative mb-8 bg-white border-b border-grey flex flex-nowrap overflow-x-auto'>
-
-        {routes.map((route,i)=>{
-            return (
-            <button ref={i===defaultActiveIndex?activeTabRef:null} key={i} className={'p-4 px-5 capitalize ' + (inPageNavIndex === i ?"text-black" : "text-dark-grey ") +(defaultHidden.includes(route)? 'md:hidden':"")} onClick={(e)=>{
-                changePageState(e.target,i)
-            }}>
-                {route}
+      <div className="relative mb-8 bg-white border-b border-grey flex flex-nowrap overflow-x-auto">
+        {routes.map((route, i) => {
+          return (
+            <button
+              ref={i === defaultActiveIndex ? activeTabRef : null}
+              key={i}
+              className={
+                "p-4 px-5 capitalize " +
+                (inPageNavIndex === i ? "text-black" : "text-dark-grey ") +
+                (defaultHidden.includes(route) ? "md:hidden" : "")
+              }
+              onClick={(e) => {
+                changePageState(e.target, i);
+              }}
+            >
+              {route}
             </button>
-            )
+          );
         })}
 
-        <hr ref={activeTabLineRef} className='absolute bottom-0 duration-300'/>
-    </div>
-    {Array.isArray(children)? children[inPageNavIndex]:children}
+        <hr ref={activeTabLineRef} className="absolute bottom-0 duration-300" />
+      </div>
+      {Array.isArray(children) ? children[inPageNavIndex] : children}
     </>
-  )
-}
+  );
+};
 
-export default InPageNavigation
+export default InPageNavigation;
--- frontend/src/pages/home.page.jsx
+++ frontend/src/pages/home.page.jsx
@@ -5,9 +5,25 @@ import axios from "axios";
 import Loader from "../components/loader.component";
 import BlogPostCard from "../components/blog-post.component";
 import MinimalBlogPost from "../components/nobanner-blog-post.component";
+import {
+  activeTabLineRef,
+  activeTabRef,
+} from "../components/inpage-navigation.component";
 const Homepage = () => {
   let [blogs, setBlog] = useState(null);
   let [trendingBlogs, setTrendingBlogs] = useState(null);
+  let [pageState, setPageState] = useState("home");
+
+  let categories = [
+    "programming",
+    "hollywood",
+    "film making",
+    "social media",
+    "cooking",
+    "tech",
+    "finances",
+    "travel",
+  ];
   const fetchLatestBlogs = () => {
     axios
       .get(import.meta.env.VITE_SERVER_DOMAIN + "/latest-blogs")
@@ -29,17 +45,33 @@ const Homepage = () => {
       });
   };
 
+  const loadBlogByCategory = (e) => {
+    console.log("click");
+    let category = e.target.innerText.toLowerCase();
+    setBlog(null);
+    if (pageState == category) {
+      setPageState("home");
+      return;
+    }
+    setPageState(category);
+  };
+
   useEffect(() => {
-    fetchLatestBlogs();
-    fetchTrendingBlogs();
-  }, []);
+    activeTabRef.current.click();
+    if (pageState == "home") {
+      fetchLatestBlogs();
+    }
+    if (!trendingBlogs) {
+      fetchTrendingBlogs();
+    }
+  }, [pageState]);
   return (
     <AnimationWrapper>
       <section className="h-cover flex justify-center gap-10">
         <div className="w-full">
           {/* {latest blogs} */}
           <InPageNavigation
-            routes={["home", "trending blogs"]}
+            routes={[pageState, "trending blogs"]}
             defaultHidden={["trending blogs"]}
           >
             <>
@@ -79,7 +111,53 @@ const Homepage = () => {
             )}
           </InPageNavigation>
         </div>
-        <div>{/* {Filters and trending blogs} */}</div>
+        <div className="min-w-[40%] lg:min-w-[400px] max-w-min border-l border-grey pl-8 pt-3 max-md:hidden ">
+          {/* {Filters and trending blogs} */}
+          <div className="flex flex-col gap-10">
+            <div>
+              <h1 className="font-medium text-xl mb-8">
+                Stories from all interests
+              </h1>
+              <div className="flex gap-3 flex-wrap">
+                {categories.map((category, i) => {
+                  return (
+                    <button
+                      key={i}
+                      className={
+                        "tag " +
+                        (pageState == category
+                          ? "bg-black text-white"
+                          : "first-letter:")
+                      }
+                      onClick={loadBlogByCategory}
+                    >
+                      {category}
+                    </button>
+                  );
+                })}
+              </div>
+            </div>
+            <div>
+              <h1 className="font-medium text-xl mb-8">
+                Trending <i className="fi fi-rr-arrow-trend-up"></i>
+              </h1>
+              {trendingBlogs == null ? (
+                <Loader />
+              ) : (
+                trendingBlogs.map((blog, i) => {
+                  return (
+                    <AnimationWrapper
+                      transition={{ duration: 1, delay: i * 0.1 }}
+                      key={i}
+                    >
+                      <MinimalBlogPost blog={blog} index={i} />
+                    </AnimationWrapper>
+                  );
+                })
+              )}
+            </div>
+          </div>
+        </div>
       </section>
     </AnimationWrapper>
   );
